6b08025b4be80ba5bae4e5b0bb5e2d70ed74140a
Akshay Kadam <akshaykadam0904@gmail.com>
2016-11-27 01:43:48 +0530

    Adding script on Sun Nov 27 01:43:48 IST 2016 :page_facing_up:

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 function pirate(obj) {
 	console.log(obj);
-	$('#app').html("<h1 class='b white tc f1'>" + obj.translation.pirate + "</h1>");
+	$('#app').html("<hr /><h1 class='b tc f1 night-hotel '>" + obj.translation.pirate + "</h1>");
+}
+
+function callPirate(text) {
+	$.ajax({
+		dataType: 'jsonp',
+  	url: "http://isithackday.com/arrpi.php?text=" + text + "&format=json"
+	})
+	.done(function(res) {
+		pirate(res);
+	})
+	.catch(function(err) {
+		if(err.statusText == "Not Found") 
+			$("#app").html("<h1 class='b white tc f1'>User Not Found</h1>");
+	});
 }
 
 $(document).ready(function() {
 	var submit = $("#form");
+	
 	submit.on("submit",function(e) {
 		e.preventDefault();
 		var text = $("#humanid").val();
 		if(text !== ""){
 			$("#humanid").val("");
-			console.log(text);
-			$.ajax({
-				dataType: 'jsonp',
-		  	url: "http://isithackday.com/arrpi.php?text=" + text + "&format=json"
-			})
-			.done(function(res) {
-				pirate(res);
-			})
-			.catch(function(err) {
-				if(err.statusText == "Not Found") 
-					$("#app").html("<h1 class='b white tc f1'>User Not Found</h1>");
-			});
+			callPirate(text);			
 		}
 	});
+
+	$('.samples').click(function() {
+		callPirate($(this)[0].innerText);
+	});
+
 });
